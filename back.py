e51f5d241ca120f739a8e01ecd61d54ab0085d33
CarolineBergholdtH <fgr164@alumni.ku.dk>
2023-05-21 09:05:53 +0200

    yes

--- back.py
+++ back.py
@@ -13,7 +13,7 @@ par.D = 2
 par.T = 25
 
 def util(N,d,par):
-    return par.eta2*N + par.eta3*(N**2) + par.mu*d
+    return par.eta2*m + par.eta3*(m**2) + par.mu*d
 
 
 def solve_last_period(par):
@@ -21,20 +21,22 @@ def solve_last_period(par):
     # a. allocate
     c_grid = np.linspace(par.m_min,par.m_max,par.m_max)
     v_func = np.empty(par.m_max)
-    c_func = np.empty(par.m_max)
+    d = (0,1)
+    d_func = np.empty(par.m_max)
 
     # b. solve
     for i,m in enumerate(c_grid):
+        for j,d in range d_func:
 
         # i. objective
-        obj = lambda x: -util(x[0],m,par)
+        obj = lambda d: -util(d,m,par)
 
         # ii. optimizer
         x0 = m/2 # initial value
-        result = optimize.minimize(obj,[x0],method='L-BFGS-B',bounds=((1e-8),))
+        result = optimize.minimize(obj,[d],method='L-BFGS-B',bounds=((1e-8),))
 
         # iii. save
         v_func[i] = -result.fun
-        c_func[i] = result.x
+        d_func[j] = result.x
         
-    return m_grid,v_func,c_func
+    return m_grid,v_func,d_func
